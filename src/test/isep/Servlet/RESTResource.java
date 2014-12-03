2bd78874e8e436b11af1f993a78501a21bbfec15
Sebastien-Trupel <sebastien.trupel@isep.fr>
2014-12-03 22:05:29 +0100

    updateData : user = OK, tweet = NOK

--- src/test/isep/Servlet/RESTResource.java
+++ src/test/isep/Servlet/RESTResource.java
@@ -12,49 +12,53 @@ import test.isep.Model.DBHelper;
 import test.isep.Model.Tweet;
 import test.isep.Model.User;
 
-@Path("/res")
-public class RESTResource 
+@Path( "/res" )
+public class RESTResource
 {
-	/*@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String testText()
-	{
-		return "just a text test";
-	}
-	
-	@GET
-	@Produces(MediaType.TEXT_XML)
-	@Path("/resxml")
-	public RESTObject testObjectXML()
-	{
-		RESTObject obj = new RESTObject();
-		obj.setAuthor("Asimov");
-		obj.setTitle("Foundation");
-		return obj;
-	}*/
-	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/users")
-	public List<User> getUsers()
-	{
-		return DBHelper.getUsers();
-	}
-	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/tweetsid/{userId}")
-	public List<Tweet> getTweetsByUserId(@PathParam("userId") long userId)
-	{
-		return DBHelper.getTweets(userId);
-	}
-	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/tweetsnickname/{nickname}")
-	public List<Tweet> getTweetsByUserNickname(@PathParam("nickname") String nickname)
-	{
-		return DBHelper.getTweets(nickname);
-	}
-	
+    /*
+     * @GET
+     * 
+     * @Produces(MediaType.TEXT_PLAIN) public String testText() { return
+     * "just a text test"; }
+     * 
+     * @GET
+     * 
+     * @Produces(MediaType.TEXT_XML)
+     * 
+     * @Path("/resxml") public RESTObject testObjectXML() { RESTObject obj = new
+     * RESTObject(); obj.setAuthor("Asimov"); obj.setTitle("Foundation"); return
+     * obj; }
+     */
+
+    @GET
+    @Produces( MediaType.APPLICATION_JSON )
+    @Path( "/users" )
+    public List<User> getUsers()
+    {
+        return DBHelper.getUsers();
+    }
+
+    @GET
+    @Produces( MediaType.APPLICATION_JSON )
+    @Path( "/tweetsid/{userId}" )
+    public List<Tweet> getTweetsByUserId( @PathParam( "userId" ) long userId )
+    {
+        return DBHelper.getTweets( userId );
+    }
+
+    @GET
+    @Produces( MediaType.APPLICATION_JSON )
+    @Path( "/tweetsnickname/{nickname}" )
+    public List<Tweet> getTweetsByUserNickname( @PathParam( "nickname" ) String nickname )
+    {
+        return DBHelper.getTweets( nickname );
+    }
+
+    @GET
+    @Path( "/update" )
+    public void updateData()
+    {
+        DBHelper.updateData();
+    }
+
 }
